9e302574dcee19735d66fb0bfbde072e33b7c8a6
yuchenyee <yuchen3188@gmail.com>
2022-08-19 00:04:03 +0800

    Update 2022-08-18T16:03:55.674Z

---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+$('.menu-icon').click(function (e) {
+  e.preventDefault();
+  $('.menu').toggleClass('active');
+});
+//# sourceMappingURL=all.js.map
